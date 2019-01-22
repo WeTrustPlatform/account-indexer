2682d148d8f749518f2663b98bec24caaa414e08
Tuyen <tuyen@wetrust.io>
2019-01-22 12:40:04 +0700

    Test tx hash, couple addr, value

--- test/e2e/get_account_transactions_e2e_test.go
+++ test/e2e/get_account_transactions_e2e_test.go
@@ -8,32 +8,44 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/big"
 	"net/http"
 	"os"
+	"strconv"
 	"testing"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
-	"github.com/WeTrustPlatform/account-indexer/common"
 	indexerHttp "github.com/WeTrustPlatform/account-indexer/http"
 	httpTypes "github.com/WeTrustPlatform/account-indexer/http/types"
 	"github.com/stretchr/testify/assert"
 )
 
+func TestConvertEther(t *testing.T) {
+	value1FromEtherScan := "0.16310815 Ether"
+	assert.Equal(t, "163108150000000000", esValueToString(value1FromEtherScan))
+	value2FromEtherScan := "9.56064602 Ether"
+	assert.Equal(t, "9560646020000000000", esValueToString(value2FromEtherScan))
+	value3FromEtherScan := "1.0182142 Ether"
+	assert.Equal(t, "1018214200000000000", esValueToString(value3FromEtherScan))
+}
+
+// Get data from etherscan.com and compare to our indexer
 func TestGetAccountTransactions(t *testing.T) {
 	// lupus master address
 	account := "0x7C419672d84a53B0a4eFed57656Ba5e4A0379084"
-	blockNumbers, eTXHashes := getDataFromEtherScan(t, account)
+	blockNumbers, esData := getDataFromEtherScan(t, account)
 	indexBlocks(t, blockNumbers)
-	iTXHashes := getTXHashesFromIndexer(t, account, len(eTXHashes))
-	assert.Equal(t, len(eTXHashes), len(iTXHashes))
-	// All Etherscan transaction appears in our indexer
-	assert.Equal(t, eTXHashes, iTXHashes)
-	// This will print out missing transaction if any
-	for _, eTX := range eTXHashes {
-		if !common.Contains(iTXHashes, eTX) {
-			assert.Fail(t, "The indexer does not contain "+eTX)
-		}
+	indexData := getDataFromIndexer(t, account, len(esData))
+	assert.Equal(t, len(esData), len(indexData))
+	// This will print out missing data if any
+	for i, esTx := range esData {
+		tx := esTx.TxHash
+		addr := esTx.CoupleAddress
+		value := esTx.Value
+		assert.Equal(t, tx, indexData[i].TxHash)
+		assert.Equal(t, addr, indexData[i].CoupleAddress)
+		assert.Equal(t, value, indexData[i].Value)
 	}
 }
 
@@ -62,7 +74,7 @@ func indexBlocks(t *testing.T, blockNumbers []string) {
 	}
 }
 
-func getTXHashesFromIndexer(t *testing.T, account string, rows int) []string {
+func getDataFromIndexer(t *testing.T, account string, rows int) []httpTypes.EIAddress {
 	url := fmt.Sprintf("http://localhost:3000/api/v1/accounts/%v?rows=%v", account, rows)
 	t.Logf("Getting index data from %v \n", url)
 	res, err := http.Get(url)
@@ -72,17 +84,13 @@ func getTXHashesFromIndexer(t *testing.T, account string, rows int) []string {
 	err = json.NewDecoder(res.Body).Decode(&httpResult)
 	assert.Nil(t, err)
 	assert.Equal(t, rows, len(httpResult.Indexes))
-	txHashes := []string{}
-	for _, item := range httpResult.Indexes {
-		txHashes = append(txHashes, item.TxHash)
-	}
-	assert.Equal(t, rows, len(txHashes))
-	return txHashes
+	return httpResult.Indexes
 }
 
-// Return list of block numbers and list of transactions
-func getDataFromEtherScan(t *testing.T, account string) ([]string, []string) {
+// Return list of block numbers and list of AddressIndex
+func getDataFromEtherScan(t *testing.T, account string) ([]string, []httpTypes.EIAddress) {
 	url := fmt.Sprintf("https://etherscan.io/txs?a=%v", account)
+	log.Printf("Getting data from %v \n", url)
 	res, err := http.Get(url)
 	assert.Nil(t, err)
 	defer res.Body.Close()
@@ -93,22 +101,46 @@ func getDataFromEtherScan(t *testing.T, account string) ([]string, []string) {
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	assert.Nil(t, err)
 	blockNumbers := []string{}
-	blockSelector := "div#ContentPlaceHolder1_mainrow > div > div > div > table > tbody > tr > td.hidden-sm > a"
-	doc.Find(blockSelector).Each(func(i int, s *goquery.Selection) {
-		text := s.Text()
-		blockNumbers = append(blockNumbers, text)
+	txLines := []httpTypes.EIAddress{}
+	trSelector := "div#ContentPlaceHolder1_mainrow > div > div > div > table > tbody > tr"
+	doc.Find(trSelector).Each(func(i int, s *goquery.Selection) {
+		// line by line
+		blockNumber := s.Find("td.hidden-sm > a").Text()
+		blockNumbers = append(blockNumbers, blockNumber)
+		txOrAddr := s.Find("td > span.address-tag > a")
+		tx := txOrAddr.First().Text()
+		addrNode := txOrAddr.Last()
+		addrHref, ok := addrNode.Attr("href")
+		assert.True(t, ok)
+		addrHrefBA := []byte(addrHref)
+		addr := string(addrHrefBA[len("/address/"):])
+
+		valueNode := s.Find("td").Last().Prev()
+		valueStr := esValueToString(valueNode.Text())
+		value := new(big.Int)
+		value.SetString(valueStr, 10)
+
+		txLines = append(txLines, httpTypes.EIAddress{
+			TxHash:        tx,
+			CoupleAddress: addr,
+			Value:         value,
+		})
 	})
 
-	txHashes := []string{}
-	txSelector := "div#ContentPlaceHolder1_mainrow > div > div > div > table > tbody > tr > td > span.address-tag > a"
-	doc.Find(txSelector).Each(func(i int, s *goquery.Selection) {
-		text := s.Text()
-		// log.Printf("text=%v", text)
-		if len(text) == 66 {
-			// transaction, not address
-			txHashes = append(txHashes, text)
-		}
-	})
+	log.Printf("Done getting data, number of transaction: %v \n", len(txLines))
 
-	return blockNumbers, txHashes
+	return blockNumbers, txLines
+}
+
+func esValueToString(str string) string {
+	valueBA := []byte(str)
+	valueStr := string(valueBA[:len(valueBA)-len(" Ether")])
+	f, _ := strconv.ParseFloat(valueStr, 64)
+	bf := big.NewFloat(f)
+	bf2 := new(big.Float).Mul(bf, big.NewFloat(1000000000000000000))
+	f64Val, _ := bf2.Float64()
+	result := strconv.FormatFloat(f64Val, 'f', 0, 64)
+	// Something wrong with the above conversion, use this as a work around
+	result = string([]byte(result)[:len(result)-5]) + "00000"
+	return result
 }
